cb921126d6d2a79dd1b607012079f119930ecd39
Mari1921 <167789893+Mari1921@users.noreply.github.com>
2024-07-04 11:20:59 -0300

    Update Desafio2.js

--- Desafio2.js
+++ Desafio2.js
@@ -1,28 +1,29 @@
 let timeheroi = ["Time dos Otakus"];
-var vitorias = 10000;
-var rankeadas = 324579;
-if (rankeadas < 10) {
-  console.log("O " + timeheroi + "Está no ranking de Ferro!");
+var vitorias = 20;
+var rankeadas = 10;
+let saldoVitorias = vitorias - rankeadas
+function mostrarNivel(Nivel) {
+    if (rankeadas < 10) {
+  console.log("O " + timeheroi + "Está com" + saldoVitorias + " e está no nivel de Ferro!");
 } else if (rankeadas > 11 || rankeadas < 20) {
-  console.log("O " + timeheroi + "Está no ranking de Bronze!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de  Bronze!");
 } else if (rankeadas > 21 || rankeadas < 50) {
-  console.log("O " + timeheroi + "Está no ranking de Prata!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de Prata!");
 } else if (rankeadas > 51 && rankeadas < 80) {
-  console.log("O " + timeheroi + "Está no ranking de Ouro!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de Ouro!");
 } else if (rankeadas > 81 && rankeadas < 90) {
-  console.log("O " + timeheroi + "Está no ranking Diamante!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de Diamante!");
 } else if (rankeadas > 91 && rankeadas < 100) {
-  console.log("O " + timeheroi + "Está no ranking Lendário!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de Lendário!");
 } else if (rankeadas >= 101) {
-  console.log("O " + timeheroi + "Está no ranking Imortal!");
+  console.log("O " + timeheroi + "Está com " + saldoVitorias + " e está no nivel de Imortal!");
 }
- function mostrarNivel(Nivel) {
-     console.log("O time dos Otaku está no nível " + Nivel + ".");
- }
- mostrarNivel(100);
+  }
+ mostrarNivel();
  
  while (rankeadas > 0) {
    rankeadas -= vitorias;
    console.log("Não desistam agora, continuem treinando!");
    break;
  }
+ 
